ed5be06600c1b3dfb6e9bec2ad8f0c924d2270d0
gyh1621 <cngyh96@gmail.com>
2018-03-11 15:42:12 +0800

    add basic user system: login and register

---
+++ web/app.py
@@ -0,0 +1,72 @@
+# !/usr/bin/python3
+# coding: utf-8
+
+
+import os
+import time
+
+from flask import Flask, render_template, request, flash, url_for, redirect
+from flask.ext.bootstrap import Bootstrap
+from flask_login import login_user, login_required
+from flask_login import current_user
+from flask_login import logout_user
+
+from __init__ import app, login_manager
+from modules import User, DataBase
+
+
+app.secret_key = os.urandom(24)
+bootstrap = Bootstrap(app)
+db = DataBase('root', 'sql2017..', 'SuperMenu')
+
+
+@app.route('/')
+def default():
+    return redirect(url_for('index'))
+
+@app.route('/index/')
+def index():
+    print(current_user)
+    return render_template(
+        'index.html',
+        user=getattr(current_user, 'username', None))
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    # 展示登录界面
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if not username:
+        flash('请填写用户名')
+        return render_template('login.html')
+    elif not password:
+        flash('请填写密码')
+        return render_template('login.html')
+    action = request.form.get('button')
+    if action == 'register':
+        # 注册
+        new_user = User(username, db)
+        new_user.password(password)
+        flash('{} 注册成功'.format(username))
+        return render_template('login.html')
+    elif action == 'login':
+        # 登录
+        user = User(username, db)
+        if user.verify_password(password):
+            login_user(user)
+            return redirect(url_for('index'))
+
+
+# 这个callback函数用于reload User object，根据session中存储的user id
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id, db)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
---
+++ web/modules.py
@@ -0,0 +1,115 @@
+# !/usr/bin/python3
+# coding: utf-8
+
+
+import pymysql
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+from flask_login import UserMixin
+from traceback import format_exc
+import json
+import uuid
+
+
+class DataBase():
+
+    def __init__(self, user, passwd, database=None,
+                 host='localhost', charset='utf8'):
+        self.db = pymysql.connect(
+            host, user, passwd, database, charset=charset)
+        self.cursor = self.db.cursor()
+
+    def execute(self, sql):
+        self.cursor.execute(sql)
+        self.db.commit()
+
+    def query_all(self, sql):
+        num = self.cursor.execute(sql)
+        if num == 0:
+            return None
+        return self.cursor.fetchall()
+
+
+class User(UserMixin):
+
+    add_user_sql = \
+        "INSERT INTO `users` (`username`, `password_hash`, `userid`) " \
+        "VALUES ('{}', '{}', '{}');"
+    get_user_sql = "SELECT * FROM `users` WHERE `{}`='{}'"
+
+    def __init__(self, username, db):
+        self.username = username
+        self.db = db
+        self.password_hash = self.get_password_hash()
+        self.id = self.get_id()
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    #@password.setter
+    def password(self, password):
+        """ save user name, id and password hash to db """
+        self.password_hash = generate_password_hash(password)
+        self.db.execute(
+            User.add_user_sql.format(
+                self.username, self.password_hash, self.id)
+        )
+
+    def verify_password(self, password):
+        if self.password_hash is None:
+            return False
+        return check_password_hash(self.password_hash, password)
+
+    def get_password_hash(self):
+        """try to get password hash from file.
+
+        :return password_hash: if the there is corresponding user in
+                the file, return password hash.
+                None: if there is no corresponding user, return None.
+        """
+        try:
+            user_info = self.db.query_all(
+                User.get_user_sql.format('username', self.username))
+            if user_info is not None:
+                return user_info[0][2]
+        except IOError:
+            return None
+        except ValueError:
+            return None
+        return None
+
+    def get_id(self):
+        """get user id from profile file, if not exist, it will
+        generate a uuid for the user.
+        """
+        if self.username is not None:
+            try:
+                userinfo = self.db.query_all(
+                    User.get_user_sql.format('username', self.username))
+                if userinfo is not None:
+                    return userinfo[0][3]
+            except IOError:
+                pass
+            except ValueError:
+                pass
+        return str(uuid.uuid4())
+
+    @staticmethod
+    def get(user_id, db):
+        """try to return user_id corresponding User object.
+        This method is used by load_user callback function
+        """
+        if not user_id:
+            return None
+        try:
+            users = db.query_all(User.get_user_sql.format('userid', user_id))
+            print(users)
+            for user in users:
+                if user[3] == user_id:
+                    return User(user[1], db)
+        except Exception as e:
+            print(str(e))
+            print(format_exc())
+            return None
+        return None
